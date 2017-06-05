b36f30d81cb4cffb0418aaa1acc47384fcbc3060
Igor Orekhov <iqor.orekhov@gmail.com>
2017-06-05 19:48:09 +0300

    still in maste

--- src/main/java/com/johnremboo/App.java
+++ src/main/java/com/johnremboo/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("still in master");
         System.out.println("new in master");
         System.out.println( "Hello World!" );
         System.out.println("Second commit");
